47393e1e79a67d668e6e5245c6f534d0b77503e9
ToddBooth <Git@ToddBooth.Com>
2023-09-18 21:38:02 +0200

    commit

--- wp-create-page.sh
+++ wp-create-page.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 user="put your username here"
-secret="put your api-key-1 here"
-website_url="https://toddbooth.com/wp-json/wp/v2"
+api_key_1="put your api-key-1 here"
+base_url="https://toddbooth.com/wp-json/wp/v2"
 
 curl -X POST \
-    -u "${user}:${secret}" \
-    "${website_url}/pages/" \
+    -u "${user}:${api_key_1}" \
+    "${base_url}/pages/" \
     -H "Content-Type: application/json" \
     -d '{
         "title": "Just a Random Page Title",
--- wp-create-post.sh
+++ wp-create-post.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 user="put your username here"
-secret="put your api-key-1 here"
-website_url="https://toddbooth.com/wp-json/wp/v2"
+api_key_1="put your api-key-1 here"
+base_url="https://toddbooth.com/wp-json/wp/v2"
 
 curl -X POST \
-	-u "${user}:${secret}" \
-	"${website_url}/posts/" \
+	-u "${user}:${api_key_1}" \
+	"${base_url}/posts/" \
 	-H "Content-Type: application/json" \
 	-d '{
 		"title": "Just A Random Post Title",
-		"content": "This is the content ofthe new post.",
+		"content": "This is the content of the new post.",
 		"status": "publish"
 	}' 
